12918dd51ad3985cb7bc47aa3d0026c65fd48c8b
yaizaaaa <yaiza0521@gmail.com>
2019-02-12 00:16:12 +0900

    共通ヘッダーの追加

---
+++ src/components/header.vue
@@ -0,0 +1,10 @@
+<template>
+  <header id="header">
+    <nav id="header-nav">
+        <router-link to="/" exact><a>ホーム</a></router-link>
+        <router-link to="/signin" exact><a>ログイン</a></router-link>
+    </nav>
+  </header>
+</template>
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,20 @@ import Router from 'vue-router'
 import signin from '@/components/signin'
 import signup from '@/components/signup'
 import authstore from '../store.js'
+import home from '@/components/home'
 
 Vue.use(Router)
 
 const router = new Router({
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: home,
+      meta: {
+        requiresAuth: false
+      }
+    },
     {
       path: '/signin',
       name: 'signin',
@@ -35,6 +44,10 @@ router.beforeEach((to, from, next) => {
   } else {
     next()
   }
+})
+
+router.afterEach((to, from) => {
+  console.log('yobaretayo!!')
 })
 
 export default router
